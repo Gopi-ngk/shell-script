6bc4afcce66518a1d2872aafd4921e1951c2b6f4
Gopi <gopikuchi1827@gmail.com>
2025-01-04 17:01:32 +0530

    runtime variables added

--- 04.variables.sh
+++ 04.variables.sh
@@ -4,9 +4,9 @@
 echo "please enter your username::"
 
 read -s USERNAME
-echo "username you entered $USERNAME::"
+echo "username you entered:: $USERNAME"
 
 echo "enter your passwor::"
 
 read -s PASSWORD
-echo "password you entered $PASSWORD::"
+echo "password you entered:: $PASSWORD"
