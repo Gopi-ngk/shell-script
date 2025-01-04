5598e9932217c205aeeed40f02f68f262d1c9bfa
Gopi <gopikuchi1827@gmail.com>
2025-01-04 15:54:28 +0530

    conversation added

--- 02.variables.sh
+++ 02.variables.sh
@@ -4,6 +4,6 @@ PERSON1=Akhil
 PERSON2=Nikhil
 
 echo "$PERSON1:: Hi $PERSON2"
-echo "$PERSON2:: Hello $PERSON2"
+echo "$PERSON2:: Hello $PERSON1"
 echo "$PERSON1:: How are you doing"
 echo "$PERSON2:: Im doing great, Thank you for asking"
