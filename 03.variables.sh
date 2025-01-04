4edbb994e7169ea65aee12dae2cebff596e7225e
Gopi <gopikuchi1827@gmail.com>
2025-01-04 16:04:37 +0530

    runtime variables added

---
+++ 03.variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hi $PERSON2"
+echo "$PERSON2:: Hello $PERSON1"
+echo "$PERSON1:: How are you doing"
+echo "$PERSON2:: Im doing great, Thank you for asking $PERSON1"
+echo "$PERSON1:: Cool"
